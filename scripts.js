826ff55b30771093f83095452986de637013ef00
Stewart Lucas <cishcabob123@gmail.com>
2021-05-06 10:33:04 -0700

    Finished hooking up all button elements to the playRound function and cleaned up code and comments. Also cleaned up CSS. Next work on making only button elements clickable.

--- scripts.js
+++ scripts.js
@@ -1,142 +1,120 @@
 let computerSelection;
-let playerSelection;
-let gameNumber;
+let playerSelection = '';
+let gameNumber = 0;
 let computerScore = 0;
 let playerScore = 0;
+let consoleOut;
+
+//Declare and add event listeners to the buttons
+const rpsButtons = document.querySelectorAll('.player-button');
+rpsButtons.forEach(button => {button.addEventListener('click', playerSelect)});
+
+//Sets player selection to the id of the clicked element
+//Calls playRound Function
+function playerSelect(e) {
+  playerSelection = e.currentTarget.id;
+  playRound();
+}
 
 //Random positive integer generator
-//Parameters are minimum and maximum ranges for the random number required
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
   }
 
 //Randomly stores either Rock, Paper or Scissors in the computerSelection variable
-//Create three strings with each name
-//Uses getRndInteger to randomly assign the string from the choices array
 function computerPlay() {
     let choices = ["rock", "paper", "scissors"];
     computerSelection = choices[getRandomInteger(0,3)];
     return;
 }
 
+//Main playRound function tests playerSelection against computerSelection
+//Calls computerPlay to get randomized rock paper or scissors
+//Calls either tieGame, playerLose, or playerWin
+function playRound() {
+  computerPlay();
 
-const rpsButtons = document.querySelectorAll('.player-button');
-
-function playerSelect(e) {
-  console.log(playerSelection);
-}
-
-
-
-//Asks the player to choose rock paper or scissors
-//Changes their selection to lower case letters
-//Alerts cancelled if the box is empty or they press cancel
-//Calls itself if the string is something other than rock paper or scissors
-/*function playerSelect(){
-  playerSelection = prompt("Choose either Rock, Paper, or Scissors!");
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection == "" || playerSelection === null){
-    console.log("Cancelled");
-    return playerSelect();
-  } else if (playerSelection == "rock" || playerSelection == "paper" || 
-      playerSelection == "scissors") {
+  //Tie Case
+  if (playerSelection == computerSelection) { //player and computer chose the same thing
+    tieGame(); 
     return;
-  } else {
-      console.log("Looks like you didn't choose rock, paper, or scissors! Please ");
-    return playerSelect();
+  }
+
+  //Rock Case
+  if (playerSelection == "rock") { //player chose rock
+    if (computerSelection == "paper"){ //computer chose paper
+      playerLose();
+    }
+    else { //computer chose scissors
+      playerWin();
+    }
+  }
+
+  //Paper Case
+  if (playerSelection == "paper") { //player chose paper
+    if (computerSelection == "scissors"){ //computer chose scissors
+      playerLose();
+    }
+    else { //computer chose rock
+      playerWin();
+    }
+  }
+
+  //Scissors Case
+  if (playerSelection == "scissors") {
+    if (computerSelection == "rock") {
+      playerLose();
+    }
+    else { //computer chose paper
+      playerWin();
+    }
   }
 }
-*/
 
 //Prints that the round is a tie
 function tieGame() {
   console.log("You and the computer both chose " + playerSelection + "!"); 
+  consoleOut = document.querySelector('#console-out-text');
+  consoleOut.textContent = "You and the computer both chose " + playerSelection +
+  "! No change in score";
 }
-//Prints that the round goes to the player
-//Prints which round it is
-//increments playerScore variable
+
+//Outputs result of the round and the score of the player
 function playerWin() {
   console.log("You win! The computer chose " + computerSelection);
-  console.log("Game number " + gameNumber + " goes to the player!");
   playerScore++;
+  gameNumber++;
+  consoleOut = document.querySelector('#console-out-text');
+  consoleOut.textContent = "You win! The computer chose " + computerSelection +
+  "! You're score is: " + playerScore;
+  console.log("Game number " + gameNumber + " goes to the you!");
+  checkWin();
 }
-//prints that the round goes to the computer
-//prints which round it is
-//increments computerScore variable
+
+//Outputs result of the round and the score of the computer
 function playerLose() {
   console.log("You Lose! The computer chose " + computerSelection);
-  console.log("Game number " + gameNumber + " goes to the computer!");
+  consoleOut = document.querySelector('#console-out-text');
   computerScore++;
+  gameNumber++;
+  consoleOut.textContent = "You lose! The computer chose " + computerSelection +
+  "! The computer's score is: " + computerScore;
+  console.log("Game number " + gameNumber + " goes to the computer!");
+  checkWin();
 }
 
-
-//Main game function
-function game() {
-
-  //Calls the playRound function 5 times
-  for (gameNumber = 1; gameNumber < 6; gameNumber++) {
-    playRound();
-  }
+//Used to check gameNumber, if its 5, displays result
+function checkWin() {
+  if (computerScore == 5 || playerScore == 5){//Runs when the score hits 5 and returns winner to console element
+    if (playerScore > computerScore) {
+      consoleOut.textContent = "You won the whole game with " + playerScore + 
+      " points";
+    } else {
+      consoleOut.textContent = "You lost with " + playerScore + 
+      " points. While the computer has " + computerScore;
+    }
+    gameNumber = 0;
+    playerScore = 0;
+    computerScore = 0;
+  } 
 }
-
-  //Tests 4 cases based on player and computer selections
-  //If equal, it's a tie and you must play again
-  //If inequal, tests player selection is win or lose
-  //return string "You Lose" , "You Win!" "You Tied, play again"
-  //incremement proper score based on win/lose
-  //decrement gameNumber if Tie to achieve 5 game result
-  function playRound() {
-    //Calls for Player input
-    //Calls for Computer input
-    computerPlay();
-  
-    //Tie Case
-    if (playerSelection == computerSelection){ //player and computer chose the same thing
-      tieGame(); 
-      gameNumber--; //Used to negate the for loops increment because the tie game does not count
-      return;
-    }
-  
-    //Rock Case
-    if (playerSelection == "rock") { //player chose rock
-      if (computerSelection == "paper"){ //computer chose paper
-        playerLose();
-        return;
-      }
-      else { //computer chose scissors
-        playerWin();
-        return;
-      }
-    }
-  
-    //Paper Case
-    if (playerSelection == "paper") { //player chose paper
-      if (computerSelection == "scissors"){ //computer chose scissors
-        playerLose();
-        return;
-      }
-      else { //computer chose rock
-        playerWin();
-        return;
-      }
-    }
-  
-    //Scissors Case
-    if (playerSelection == "scissors") {
-      if (computerSelection == "rock") {
-        playerLose();
-        return;
-      }
-      else { //computer chose paper
-        playerWin();
-        return;
-      }
-    }
-  }
-
-//Tests who won based on who had the higher score
-if (playerScore > computerScore){
-  console.log("You won against the Computer!");
-} else {
-    console.log("The computer has won :((((");
-  }
